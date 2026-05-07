7f3d533e0af190e615ea62595642ae93b63b8638
Maksim Kauger <136794025+ITactics@users.noreply.github.com>
2026-05-07 11:10:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -402,8 +402,7 @@ if not df.empty:
     
     # with c1: st.table(ldf.iloc[:mid].reset_index(drop=True))
     # with c2: st.table(ldf.iloc[mid:].reset_index(drop=True))
-    # st.dataframe(ldf, use_container_width=True, hide_index=True)
-    st.table(ldf)
+    st.dataframe(ldf, use_container_width=True, hide_index=True)
 
     st.markdown("---")
 
